230fb5e6265e284e61e0a49425ad234ce8ed8138
Lucas Mauger <lucas.mauger@gmail.com>
2021-10-17 17:12:17 +0200

    Première gestion (non finie) des élèves dispensés.

--- equipes.py
+++ equipes.py
@@ -3,19 +3,8 @@ import csv
 from pathlib import Path
 from glob import glob
 
-nb_equipes_input = input("Combien d'équipes pour la répartition ? (répondre en chiffres) : ")
-while not nb_equipes_input.isnumeric():
-    nb_equipes_input = input("Veuillez répondre en chiffres. Combien d'équipes pour la répartition ? ")
-nb_equipes = int(nb_equipes_input)
-fichier_eleves = "liste_eleves.csv"
-
 # liste globale de toutes les équipes
 toutes_eq = []
-
-# on initialise la liste pour que la fonction index_plus_petite_equipe fonctionne
-for i in range(0,nb_equipes):
-    equipe = []
-    toutes_eq.append(equipe)
 
 def index_plus_petite_equipe(liste_equipes) :
     ''' Retourne l'index de l'équipe comptant le moins de joueurs '''
@@ -39,6 +28,18 @@ def couleur_equipe(numero_equipe):
     else:
         numero_equipe = numero_equipe
     return numero_equipe
+
+def ajouter_eleve_dispense(fichier_eleves, liste_eleves_dispenses):
+    with open(fichier_eleves,newline='') as db_eleves:
+        lecteur = csv.reader(db_eleves,delimiter=';')
+        prenom_eleve = input("Veuillez entrer le prénom (ou les premières lettres du prénom) d'un élève à dispenser du tirage : ")
+        while not prenom_eleve.isalpha():
+            prenom_eleve = input("Veuillez ne pas entrer de chiffres. Prénom (ou début de prénom) d'un élève à dispenser ? ")
+
+        for eleve in lecteur:
+            if prenom_eleve in eleve[1]:
+                liste_eleves_dispenses.append(eleve)
+    return liste_eleves_dispenses
 
 def attribuer_joueur(liste_joueurs):
     ''' Attribue chaque joueur d'une liste de joueurs à l'équipe comportant
@@ -91,7 +92,7 @@ def programme_principal(fichier_eleves_csv):
     path = Path('./tirage_equipes')
     path.mkdir(exist_ok=True)
 
-    with open('./tirage_equipes/equipes_eleves.csv', 'w', newline='',encoding='utf-8') as equipes_attribuees:
+    with open('./tirage_equipes/equipes_eleves.csv', 'w', newline='',encoding='ansi') as equipes_attribuees:
         scripteur = csv.writer(equipes_attribuees, delimiter=';')
         scripteur.writerow(['nom','prenom','equipe','prof'])
         for equipe in toutes_eq:
@@ -110,13 +111,53 @@ if len(liste_fichiers_csv)>1:
     print("Veuillez ne conserver que le fichier qui contient la liste des élèves à répartir, puis relancez le programme.")
 else:
     print(f"Le fichier \"{liste_fichiers_csv[0][2:]}\" a été trouvé dans le répertoire.")
-    reponse_bool = input("Voulez-vous l'utiliser pour la répartition (o/n) ? ")
-    while reponse_bool not in ['o','n']:
-        reponse_bool = input(f"Je n'ai pas compris votre réponse.\nVoulez-vous utiliser le fichier \"{liste_fichiers_csv[0][2:]}\" pour la répartition ? (o/n)")
-    if reponse_bool == 'n':
+    reponse_fichier_eleves = input("Voulez-vous l'utiliser pour la répartition (o/n) ? ")
+    while reponse_fichier_eleves not in ['o','n']:
+        reponse_fichier_eleves = input(f"Je n'ai pas compris votre réponse.\nVoulez-vous utiliser le fichier \"{liste_fichiers_csv[0][2:]}\" pour la répartition ? (o/n)")
+    if reponse_fichier_eleves == 'n':
         print("Les équipes n'ont pas été réparties.\nFin du programme.")
     else:
         fichier_eleves = f'{liste_fichiers_csv[0][2:]}'
-        programme_principal(fichier_eleves)
-        print("Répartition des équipes effectuée.\nVous la trouverez dans le dossier \"tirage_equipes\"")
-        input("Appuyez sur Entrée pour quitter le programme.")
+        nb_equipes_input = input("Combien d'équipes pour la répartition ? (répondre en chiffres) : ")
+        while not nb_equipes_input.isnumeric():
+            nb_equipes_input = input("Veuillez répondre en chiffres. Combien d'équipes pour la répartition ? ")
+        nb_equipes = int(nb_equipes_input)
+        # on initialise la liste pour que la fonction index_plus_petite_equipe fonctionne
+        for i in range(0,nb_equipes):
+            equipe = []
+            toutes_eq.append(equipe)
+
+        liste_eleves_dispenses = []
+        reponse_dispense = input("Y a-t-il  des élèves à dispenser avant de procéder au tirage (o/n) ? ")
+        while reponse_dispense not in ['o','n']:
+            nb_equipes_input = input("Je n'ai pas compris votre réponse.\nY a-t-il  des élèves à dispenser avant de procéder au tirage (o/n) ? ")
+        
+        #on établit une liste des élèves à dispenser du tirage
+        while reponse_dispense=='o':
+            liste_eleves_dispenses = ajouter_eleve_dispense(fichier_eleves, liste_eleves_dispenses)
+            if len(liste_eleves_dispenses) == 0:
+                print('Aucun élève trouvé pour cette saisie.')
+            else:
+                if len(liste_eleves_dispenses) == 1:
+                    print("L'élève suivant a été trouvé :")
+                    print(f"{liste_eleves_dispenses[0][1]} {liste_eleves_dispenses[0][0]}")
+                else :
+                    print("Les élèves suivants ont été trouvés :")
+                    x=1
+                    for eleve_trouve in liste_eleves_dispenses:
+                        print(f"{x} - {eleve_trouve[1]} {eleve_trouve[0]}")
+                        x+=1
+            reponse_dispense = input("Voulez-vous ajouter un autre élève à dispenser (o/n) ? ")
+            while reponse_dispense not in ['o','n']:
+                reponse_dispense = input("Je n'ai pas compris votre réponse. Voulez-vous ajouter un autre élève à dispenser (o/n) ? ")
+
+        # si on ne dispense aucun élève au préalable :
+        if reponse_dispense == 'n':
+
+            print("Les élèves suivants ne feront pas partie du tirage des équipes :")
+            for eleve in liste_eleves_dispenses:
+                print(f"- {eleve[1]} {eleve[0]} -")
+
+            programme_principal(fichier_eleves)
+            print("Répartition des équipes effectuée.\nVous la trouverez dans le dossier \"tirage_equipes\"")
+            input("Appuyez sur Entrée pour quitter le programme.")
